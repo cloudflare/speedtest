c5c6600e04bac17a643bdef1497d3986396891f4
John Carmichael <john@johnine.org>
2023-11-01 11:17:13 +0000

    split out the summery reponse type to allow independant use

--- src/index.d.ts
+++ src/index.d.ts
@@ -112,22 +112,24 @@ export interface ResultDownload {
   timings: ResultDownloadTiming[]
 }
 
+export interface GetSummary {
+  download?: number,
+  upload?: number,
+  latency?: number,
+  jitter?: number,
+  downLoadedLatency?: number,
+  downLoadedJitter?: number,
+  upLoadedLatency?: number,
+  upLoadedJitter?: number,
+  packetLoss?: number,
+}
+
 export declare class Results {
   constructor ();
 
   readonly isFinished: boolean;
 
-  getSummary: () => {
-    download?: number,
-    upload?: number,
-    latency?: number,
-    jitter?: number,
-    downLoadedLatency?: number,
-    downLoadedJitter?: number,
-    upLoadedLatency?: number,
-    upLoadedJitter?: number,
-    packetLoss?: number,
-  };
+  getSummary: () => GetSummary;
 
   getUnloadedLatency: () => number | undefined;
   getUnloadedJitter: () => number | undefined;
